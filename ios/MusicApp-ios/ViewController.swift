1e8164d123049f7f9f5a57eee58be234cacd8bf4
Jacob Weate <jweate@vt.edu>
2018-03-29 17:21:38 -0400

    Play, Pause and Skip functionality

--- ios/MusicApp-ios/ViewController.swift
+++ ios/MusicApp-ios/ViewController.swift
@@ -16,18 +16,21 @@ class ViewController: UIViewController, SPTAudioStreamingPlaybackDelegate, SPTAu
     var player: SPTAudioStreamingController?
     var loginUrl: URL?
     
+    /*
+     * Main
+     */
     override func viewDidLoad() {
         super.viewDidLoad()
-        os_log("setting up")
         setup()
-        os_log("calling first login")
         NotificationCenter.default.addObserver(self, selector: #selector(updateAfterFirstLogin), name: Notification.Name("LoggedIn"), object: nil)
         
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    /*
+     * Called when user originally logins in - creates SPT Player
+     */
     @objc func updateAfterFirstLogin () {
-        os_log("updated after first")
         if let sessionObj:AnyObject = UserDefaults.standard.object(forKey: "SpotifySession") as AnyObject? {
             let sessionDataObj = sessionObj as! Data
             let firstTimeSession = NSKeyedUnarchiver.unarchiveObject(with: sessionDataObj) as! SPTSession
@@ -35,51 +38,81 @@ class ViewController: UIViewController, SPTAudioStreamingPlaybackDelegate, SPTAu
             initializePlayer(authSession: session)
         }
     }
-        
-        func initializePlayer(authSession:SPTSession){
-            os_log("initialized")
-            if self.player == nil {
-                self.player = SPTAudioStreamingController.sharedInstance()
-                self.player!.playbackDelegate = self
-                self.player!.delegate = self
-                try! player!.start(withClientId: auth.clientID)
-                self.player!.login(withAccessToken: authSession.accessToken)
-            }
+    
+    /*
+     * Initializes the Spotify Player
+     */
+    func initializePlayer(authSession:SPTSession){
+        if self.player == nil {
+            self.player = SPTAudioStreamingController.sharedInstance()
+            self.player!.playbackDelegate = self
+            self.player!.delegate = self
+            try! player!.start(withClientId: auth.clientID)
+            self.player!.login(withAccessToken: authSession.accessToken)
         }
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    /*
+     * Sets up credentials of our Application
+     */
     func setup(){
         SPTAuth.defaultInstance().clientID = "e442cd6018ed442793042372fc7a1b1e"
         SPTAuth.defaultInstance().redirectURL = URL(string: "MusicApp-ios://returnAfterLogin")
         SPTAuth.defaultInstance().requestedScopes = [SPTAuthStreamingScope, SPTAuthPlaylistReadPrivateScope, SPTAuthPlaylistModifyPublicScope, SPTAuthPlaylistModifyPrivateScope]
         loginUrl = SPTAuth.defaultInstance().spotifyWebAuthenticationURL()
-        os_log("set up complete")
     }
     
-//    uncommenting will play music on sign in 
-//    func audioStreamingDidLogin(_ audioStreaming: SPTAudioStreamingController!) {
-//        // after a user authenticates a session, the SPTAudioStreamingController is then initialized and this method called
-//        print("logged in")
-//        self.player?.playSpotifyURI("spotify:track:58s6EuEYJdlb0kO7awm3Vp", startingWith: 0, startingWithPosition: 0, callback: { (error) in
-//            if (error != nil) {
-//                print("playing!")
-//            }
-//        })
-//    }
 
     @IBOutlet weak var SignIn: UIButton!
+    /*
+     * Function called when Sign In button is pressed
+     */
     @IBAction func SignIn(_ sender: UIButton) {
         if UIApplication.shared.openURL(loginUrl!) {
-            os_log("sign in")
             if auth.canHandle(auth.redirectURL) {
                 // To do - build in error handling
             }
         }
     }
+   
+   /*
+    * Function called when Play button is pressed
+    */
+    @IBAction func Play(_ sender: UIButton) {
+
+        self.player?.playSpotifyURI("spotify:track:58s6EuEYJdlb0kO7awm3Vp", startingWith: 0, startingWithPosition: 0, callback: { (error) in
+            if (error != nil) {
+                print("playing!")
+            }
+        })
+        
+    }
     
+    /*
+     * Function called when Pause button is pressed
+     */
+    @IBAction func Pause(_ sender: UIButton) {
+        self.player?.setIsPlaying(false ,callback: { (error) in
+            if (error != nil) {
+                print("playing!")
+            }
+        })
+    }
+    
+    /*
+     * Function called when Skip is pressed
+     */
+    @IBAction func Skip(_ sender: Any) {
+        self.player?.playSpotifyURI("spotify:track:6JzzI3YxHCcjZ7MCQS2YS1", startingWith: 0, startingWithPosition: 0, callback: { (error) in
+            if (error != nil) {
+                print("playing!")
+            }
+        })
+    }
 }
 
