a44cbe9586a82e77dbf87c2592e40638dd1536d2
Vincent Eastman <vincee7@vt.edu>
2018-04-29 06:51:05 -0400

    commiting

--- MusicAppAPI/src/main/java/com/example/demo/AController.java
+++ MusicAppAPI/src/main/java/com/example/demo/AController.java
@@ -43,10 +43,96 @@ public class AController {
 	
 	@RequestMapping(value = "/getRecs", method = RequestMethod.GET)
 	@ResponseBody
-	public List<Track> getRecs(@RequestParam("access_token") String token) {
-		return dal.getRecs(token);
+	public List<Track> getRecs(@RequestParam("access_token") String token,
+								@RequestParam("user_id") String id) {
+		return dal.getRecs(token, id);
 	}
 	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//	@RequestMapping(value = "/getUsers", method = RequestMethod.GET)
+//	@ResponseBody
+//	public List<User> getAllUsers(){
+//		return dal.getAllUsers();
+//	}
+//	
+//	@RequestMapping(value = "/getJobs", method = RequestMethod.GET)
+//	@ResponseBody
+//	public List<Job> getJobs(@RequestParam("idUsers") int idUsers){
+//		return dal.getJobs(idUsers);
+//	}
+//	
+//	@RequestMapping(value = "/getUserInfo", method = RequestMethod.GET)
+//	@ResponseBody
+//	public User getUserInfo(String email){
+//		return dal.getUserInfo(email);
+//	}
+//	
+//	@RequestMapping(value = "/addUser", method = RequestMethod.POST)
+//	@ResponseBody
+//	public String addUser(@RequestParam("FirstName") String FirstName,
+//							@RequestParam("LastName") String LastName,
+//							@RequestParam("Address") String Address,
+//							@RequestParam("Phone") String Phone,
+//							@RequestParam("Email") String Email,
+//							@RequestParam("BirthDate") String BirthDate,
+//							@RequestParam("Password") String Password) {
+//		User user = new User();
+//		user.setFirstName(FirstName);
+//		user.setLastName(LastName);
+//		user.setAddress(Address);
+//		user.setPhone(Phone);
+//		user.setEmail(Email);
+//		user.setBirthDate(BirthDate);
+//		user.setPassword(Password);
+//		
+//	
+//		return dal.addUser(user);
+//	}
+//	
+//	@RequestMapping(value = "/verifyUser", method = RequestMethod.GET)
+//	@ResponseBody
+//	public String verifyUser(@RequestParam("email") String email, 
+//							@RequestParam("password") String password) {
+//		return dal.verifyUser(email, password);
+//		
+//	}
+//	
+//	@RequestMapping(value = "/updateUser", method = RequestMethod.POST)
+//	@ResponseBody
+//	public String updateUser(@RequestParam("FirstName") String FirstName,
+//							@RequestParam("LastName") String LastName,
+//							@RequestParam("Address") String Address,
+//							@RequestParam("Phone") String Phone,
+//							@RequestParam("Email") String Email,
+//							@RequestParam("BirthDate") String BirthDate,
+//							@RequestParam("Password") String Password,
+//							@RequestParam("idUsers") int idUsers ){
+//		
+//		User user = new User();
+//		user.setFirstName(FirstName);
+//		user.setLastName(LastName);
+//		user.setAddress(Address);
+//		user.setPhone(Phone);
+//		user.setEmail(Email);
+//		user.setBirthDate(BirthDate);
+//		user.setPassword(Password);
+//		user.setIdUsers(idUsers);
+//		
+//	
+//		return dal.updateUser(user);
+//	}
 	
 	
 	
